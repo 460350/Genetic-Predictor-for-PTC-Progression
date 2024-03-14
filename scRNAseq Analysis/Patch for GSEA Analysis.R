2a8cedb7c5d34afb49193b793ac8db21e25a4d4e
460350 <100200976+460350@users.noreply.github.com>
2024-03-14 19:16:17 +0800

    Create Patch for GSEA Analysis.R

---
+++ scRNAseq Analysis/Patch for GSEA Analysis.R
@@ -0,0 +1,6 @@
+# The GSEA conducted here is totally based on unfiltered DEGs and new rank algorithm
+# Two figures are about GSEA results from two Scissor analyses and MAPK13, STMN1, HMGB2 three-gene model GSEA
+####original code####
+###Scissor analysis part###
+
+###Three-gene model###
